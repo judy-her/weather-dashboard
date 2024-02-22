ee95ac9e377dc7c51bfcc1fb1782c7e2cf8b4651
judy_her <judith85dm@ucla.edu>
2024-02-22 13:13:42 -0800

    added fetch queryURL

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 console.log('I am connected :)');
 const APIKey = 'f4d2ec0aceb6adaf6e9866e242642310';
 
-const url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}"
+// const url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}"
 
-let city;
+let city = 'los angeles';
+const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-const weatherApp = {
-  init: () => {
-    document
-      .getElementById('search-btn')
-      .addEventListener('click', weatherApp.fetchWeather);
-    document
-      .getElementById('btnCurrent')
-      .addEventListener('click', weatherApp.getLocation);
-  },
-function fetchWeather() {
+fetch(queryURL)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Error', error));
 
-},
-//geolocation 
-function getLocation (ev) {
-    let opts = {
-        enableHighAccuracy: true,
-        timout: 1000 * 10, //10 seconds
-        maximumAge: 1000 * 60 * 5 //5 minutes
-    }
-    navigator.geolocation.getCurrentPosition(app.ftw, app.wtf,opts)
+// const weatherApp = {
+//   init: () => {
+//     document
+//       .getElementById('search-btn')
+//       .addEventListener('click', weatherApp.fetchWeather);
+//     document
+//       .getElementById('btnCurrent')
+//       .addEventListener('click', weatherApp.getLocation);
+//   },
+// function fetchWeather() {
 
-},
-ftw: (position)=>{
+// },
+// //geolocation
+// function getLocation (ev) {
+//     let opts = {
+//         enableHighAccuracy: true,
+//         timout: 1000 * 10, //10 seconds
+//         maximumAge: 1000 * 60 * 5 //5 minutes
+//     }
+//     navigator.geolocation.getCurrentPosition(app.ftw, app.wtf,opts)
 
-},
-wtf: (err)=>{
+// },
+// ftw: (position)=>{
 
-},
-function showWeather (){
+// },
+// wtf: (err)=>{
 
-},
- 
-};
+// },
+// function showWeather (){
+
+// },
+
+// };
