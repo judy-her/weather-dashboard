77d16b18dfb16584490ba8c0051aeb45d67e0b3a
judy_her <judith85dm@ucla.edu>
2024-03-22 12:43:30 -0700

    added get forecast data and passed lat and lon to it

--- script.js
+++ script.js
@@ -6,65 +6,38 @@ let query = 'london';
 let units = 'imperial';
 const weatherBASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=${units}`;
 
+const cnt = 5; //for 5 days
+
 const getWeatherData = () => {
   return fetch(weatherBASE_URL).then((res) => res.json());
 };
 
-getWeatherData().then((weatherData) => {
-  const {
-    coord,
-    weather,
-    main,
-    wind,
-    dt,
-    sys,
-    timezone,
-    id,
-    name,
-  } = weatherData;
-
-  const weatherItems = {
-    coord: {
-      lon: coord.lon,
-      lat: coord.lat,
-    },
-    weather: {
-      id: weather[0].id,
-      main: weather.main,
-      description: weather.description,
-      icon: weather[0].icon,
-    },
-    wind: {
-      speed: wind.speed,
-    },
-    dt,
-    sys: {
-      type: sys.type,
-      id: sys.id,
-      country: sys.country,
-      sunrise: sys.sunrise,
-      sunset: sys.sunset,
-    },
-    timezone,
-    id,
-    name,
-  };
-  console.log(weatherItems);
-  console.log(weatherItems);
-  console.log('City:', name);
-  console.log('lat:', coord.lat);
-  console.log('lon:', coord.lon);
-  console.log('Country:', sys.country);
-  console.log('Temparature:', main.temp);
-  console.log('Humidity:', main.humidity);
-  console.log('Wind:', wind.speed, 'mph');
-  console.log('Feels Like:', main.feels_like);
-
-  //use this lat and lon for api forecast
-  let cnt = 5;
-  let lat = coord.lat;
-  let lon = coord.lon;
+const getForecastData = (lat, lon) => {
+  //forecast url
   const forecastBASE_URL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}&cnt=${cnt}&lat=${lat}&lon=${lon}`;
+  return fetch(forecastBASE_URL)
+    .then((res) => res.json())
+    .then(console.log('Forecast API', forecastBASE_URL));
+};
 
-  console.log('Forecast API URL:', forecastBASE_URL);
+getWeatherData().then((weatherData) => {
+  const { coord } = weatherData;
+  const { lat, lon } = coord;
+
+  console.log('Weather Data:', weatherData);
+  console.log('lat:', lat);
+  console.log('lon:', lon);
+
+  getForecastData(lat, lon).then((forecastData) => {
+    console.log('Forecast Data', forecastData);
+  });
 });
+
+//use this lat and lon for api forecast
+//   let cnt = 5;
+//   let lat = coord.lat;
+//   let lon = coord.lon;
+//   const forecastBASE_URL = `https://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}&cnt=${cnt}&lat=${lat}&lon=${lon}`;
+
+//   console.log('Forecast API URL:', forecastBASE_URL);
+// });
